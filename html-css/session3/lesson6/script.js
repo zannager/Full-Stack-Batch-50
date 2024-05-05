4fcf040c2bb1ffe67f7981e149fee3a5744404a2
zannager <gzraenga@gmail.com>
2024-05-05 15:36:42 +0800

    loops

---
+++ html-css/session3/lesson6/script.js
@@ -0,0 +1,17 @@
+//for loop
+for (let i = 0; i < 5; i++) {
+    console.log("i = " + i)
+}
+
+//while
+let i = 0;
+let j = 0
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+do {
+    console.log("j = " + j);
+    j++;
+} while (j < 5)

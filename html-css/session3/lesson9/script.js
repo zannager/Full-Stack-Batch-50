6bd2beb7288ac01e517038aa17a2c12863df59b5
zannager <gzraenga@gmail.com>
2024-05-05 17:14:04 +0800

    DOM and events

--- html-css/session3/lesson9/script.js
+++ html-css/session3/lesson9/script.js
@@ -1,4 +1,31 @@
 let button = document.getElementById("btn1")
-console.log(button.style)
+        console.log(button.style)
+        // background-color: 
+        button.style.backgroundColor = "red"
+        button.style.fontSize = "100px"
 
-button.style.backgroundColor = "red";
+//  let newButton = document.createElement('button')
+//         newButton.textContent = "new button"
+//         let body = document.querySelector('body')
+//         let btn1 = document.querySelector('#btn1')
+//         body.appendChild(newButton)
+
+//         console.log(btn1)
+
+//         let btn1 = document.querySelector('#btn1')
+//         let body = document.querySelector('body')
+
+//         let toggle = false;
+//         btn1.addEventListener('click', function(){
+
+//             if(toggle){
+//                 body.style.backgroundColor = 'red'
+//             }else{
+//                 body.style.backgroundColor = '#fff'
+//             }
+//             toggle = !toggle
+//         })
+
+//         function clickMe(){
+//             console.log('clicked!')
+//         }

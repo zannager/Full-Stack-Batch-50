085f2eb168b8b1171c5752b68075701148fc75a7
zannager <gzraenga@gmail.com>
2024-05-05 16:00:39 +0800

    function

---
+++ html-css/session3/lesson7/script.js
@@ -0,0 +1,15 @@
+function printHelloWorld10x() {
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+    console.log("hellow world");
+}
+
+printHelloWorld10x();
+printHelloWorld10x();

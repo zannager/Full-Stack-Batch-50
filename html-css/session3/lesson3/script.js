feb70a759b5cdd7c018315c272942e7889adedaf
zannager <gzraenga@gmail.com>
2024-05-05 14:32:52 +0800

    comparison operator

---
+++ html-css/session3/lesson3/script.js
@@ -0,0 +1,53 @@
+let a = 10;
+        // let a = 10;
+        // a += 2; //  a = a + 2;
+        // a -= 10 //  a = a - 10;
+        // a *= 10 //  a = a * 10;
+        // a /= 10 //  a = a / 10;
+
+        let b = 2;
+        // let b = 2;
+        // b += 3; // b = 2 + 3
+        // b += 3; // b = 5 + 3
+        // b += 3; // b = 8 + 3 
+
+        // console.log('b = ' + b); // 11
+
+        let c = 5;
+        // let c = 5;
+
+        // a -= c; // a = 10 - 5  === 5
+        // a *= b; // a = 5 * 2   === 10
+
+        // a++; // increment value by 1
+        // c--; // decrement value by 1
+
+        --a;
+        ++b;
+        // --a;
+        // ++b;
+
+        console.log('++b = ', b)
+        console.log('--a = ', a)
+        // console.log('++b = ', b)
+        // console.log('--a = ', a)
+
+        let x = 10 // number
+        let y = 5
+        // '10'
+        // 10
+        // x == 10
+
+        console.log(x == '10') // true // typecast
+        console.log(x != "10") // false 
+
+        console.log(x === '10') // false
+        console.log(x !== '10') // true
+
+        console.log(x > y) // true
+        console.log(x < y) // false
+        console.log(x <= y) // false
+        console.log(x >= y) // true
+
+
+        // x === '10'

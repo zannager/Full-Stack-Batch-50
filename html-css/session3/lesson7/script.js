8fdf9e575aec14f2fd6ed2499b75c35c169b6995
zannager <gzraenga@gmail.com>
2024-05-05 16:20:17 +0800

    function

--- html-css/session3/lesson7/script.js
+++ html-css/session3/lesson7/script.js
@@ -4,12 +4,21 @@ function printHelloWorld10x() {
     console.log("hellow world");
     console.log("hellow world");
     console.log("hellow world");
-    console.log("hellow world");
-    console.log("hellow world");
-    console.log("hellow world");
-    console.log("hellow world");
-    console.log("hellow world");
+    
 }
 
 printHelloWorld10x();
 printHelloWorld10x();
+
+function greetings(name) {
+    console.log(`hello ${name}`)
+}
+
+greetings("roy")
+let val = greetings("juan")
+console.log(val)
+
+function sum(x, y) {
+    return x + y
+}
+console.log(sum(1,2));

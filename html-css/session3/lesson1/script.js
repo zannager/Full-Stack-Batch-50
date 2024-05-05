d61c643bfc00939e84a8a997231551df60aa3a43
zannager <gzraenga@gmail.com>
2024-05-05 14:26:22 +0800

    arithmetic operators

---
+++ html-css/session3/lesson1/script.js
@@ -0,0 +1,17 @@
+// arithmetic operators
+        // + - / *
+        let x = 5;
+        let y = 2;
+
+        let sum = x + y;
+        let difference = x - y;
+        let quotient = x / y;
+        let product = x * y;
+        let remainder = x % y;
+
+
+        console.log('sum: ', sum);
+        console.log('difference: ', difference);
+        console.log('quotient: ', quotient);
+        console.log('product: ', product);
+        console.log('remainder: ', remainder);

475df03ec7f51a426af61989ed47895e215f08b3
zannager <gzraenga@gmail.com>
2024-05-05 14:48:49 +0800

    conditional statement

---
+++ html-css/session3/lesson5/script.js
@@ -0,0 +1,29 @@
+//falsy
+
+console.log(false);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+if (false) {
+    console.log("hello world");
+}
+
+let a = 10;
+let b = 2;
+
+if (a > b) {
+    console.log("a is less than b");
+}
+
+else if (b > a) {
+    console.log("b is greater than a");
+}
+
+else {
+    console.log("no condition above is true");
+}

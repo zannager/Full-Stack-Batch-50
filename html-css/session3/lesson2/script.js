62aab1cb39f1f014883bdeaee1e24f01dc399988
zannager <gzraenga@gmail.com>
2024-05-05 14:30:19 +0800

    assignment operator

---
+++ html-css/session3/lesson2/script.js
@@ -0,0 +1,67 @@
+// arithmetic operators
+        // + - / *
+        let x = 5;
+        let y = 2;
+        // let x = 10;
+        // let y = 2;
+        // let z = 8;
+
+        let sum = x + y;
+        let difference = x - y;
+        let quotient = x / y;
+        let product = x * y;
+        let remainder = x % y;
+        // let sum = x + y;
+        // let difference = x - y;
+        // let quotient = x / y;
+        // let product = x * y;
+        // let remainder = x % y;
+
+
+        // console.log('sum: ', sum);
+        // console.log('difference: ', difference);
+        // console.log('quotient: ', quotient);
+        // console.log('product: ', product);
+        // console.log('remainder: ', remainder);
+
+        // console.log('output:', (x - y )/(z + x))
+        // 10 - 2 / 8 + 10
+
+
+        // assignment operators
+        /* 
+        this is a comment
+        */
+        let a = 10;
+        // a += 2; //  a = a + 2;
+        // a -= 10 //  a = a - 10;
+        // a *= 10 //  a = a * 10;
+        // a /= 10 //  a = a / 10;
+
+        let b = 2;
+        // b += 3; // b = 2 + 3
+        // b += 3; // b = 5 + 3
+        // b += 3; // b = 8 + 3 
+
+        // console.log('b = ' + b); // 11
+
+        let c = 5;
+
+        // a -= c; // a = 10 - 5  === 5
+        // a *= b; // a = 5 * 2   === 10
+
+        // a++; // increment value by 1
+        // c--; // decrement value by 1
+
+        --a;
+        ++b;
+
+        console.log('++b = ', b)
+        console.log('--a = ', a)
+
+
+        console.log('sum: ', sum);
+        console.log('difference: ', difference);
+        console.log('quotient: ', quotient);
+        console.log('product: ', product);
+        console.log('remainder: ', remainder);

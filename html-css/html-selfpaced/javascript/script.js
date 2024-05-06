13a80c539af2ece958e9dcc9ce11f105a390942a
zannager <gzraenga@gmail.com>
2024-05-06 11:30:26 +0800

    practice - basic JS

--- html-css/html-selfpaced/javascript/script.js
+++ html-css/html-selfpaced/javascript/script.js
@@ -1,3 +1,15 @@
 function displayDate() {
-    document.getElementById('demo').innerHTML = Date();
+    document.getElementById("demo").innerHTML = Date();
 }
+
+function demo2() {
+    document.getElementById("demo2").innerHTML = "here is the new paragraph";
+    document.getElementById("demo2").style.backgroundColor = 'red';
+}
+
+function printHelloWorld() {
+    document.getElementById("demo3").innerHTML = "Hello world.";
+}
+document.addEventListener("DOMContentLoaded", function() {
+    printHelloWorld();
+});

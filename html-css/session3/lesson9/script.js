2ddb407dbde5003e0a1f74e9ce4a831d4999873e
zannager <gzraenga@gmail.com>
2024-05-06 10:13:51 +0800

    button

--- html-css/session3/lesson9/script.js
+++ html-css/session3/lesson9/script.js
@@ -1,8 +1,8 @@
-let button = document.getElementById("btn1")
-        console.log(button.style)
-        // background-color: 
-        button.style.backgroundColor = "red"
-        button.style.fontSize = "100px"
+// let button = document.getElementById("btn1")
+//         console.log(button.style)
+//         // background-color: 
+//         button.style.backgroundColor = "red"
+//         button.style.fontSize = "100px"
 
 //  let newButton = document.createElement('button')
 //         newButton.textContent = "new button"
@@ -12,20 +12,20 @@ let button = document.getElementById("btn1")
 
 //         console.log(btn1)
 
-//         let btn1 = document.querySelector('#btn1')
-//         let body = document.querySelector('body')
+        let btn1 = document.querySelector('#btn1')
+        let body = document.querySelector('body')
 
-//         let toggle = false;
-//         btn1.addEventListener('click', function(){
+        let toggle = false;
+        btn1.addEventListener('click', function(){
 
-//             if(toggle){
-//                 body.style.backgroundColor = 'red'
-//             }else{
-//                 body.style.backgroundColor = '#fff'
-//             }
-//             toggle = !toggle
-//         })
+            if(toggle){
+                body.style.backgroundColor = 'red'
+            }else{
+                body.style.backgroundColor = '#fff'
+            }
+            toggle = !toggle
+        })
 
-//         function clickMe(){
-//             console.log('clicked!')
-//         }
+        function clickMe(){
+            console.log('clicked!')
+        }

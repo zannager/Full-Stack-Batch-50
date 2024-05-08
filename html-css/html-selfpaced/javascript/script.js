24f608140bbe0f97d793aeea1f03970c78646875
zannager <gzraenga@gmail.com>
2024-05-08 22:40:08 +0800

    practice - basic JS

--- html-css/html-selfpaced/javascript/script.js
+++ html-css/html-selfpaced/javascript/script.js
@@ -1,27 +1,39 @@
-function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
-}
+// function displayDate() {
+//     document.getElementById("demo").innerHTML = Date();
+// }
 
-function demo2() {
-    document.getElementById("demo2").innerHTML = "here is the new paragraph";
-    document.getElementById("demo2").style.backgroundColor = 'red';
-}
+// function demo2() {
+//     document.getElementById("demo2").innerHTML = "here is the new paragraph";
+//     document.getElementById("demo2").style.backgroundColor = 'red';
+// }
 
-function printHelloWorld() {
-    document.getElementById("demo3").innerHTML = "Hello world.";
-}
-document.addEventListener("DOMContentLoaded", function() {
-    printHelloWorld();
-});
+// function printHelloWorld() {
+//     document.getElementById("demo3").innerHTML = "Hello world.";
+// }
+// document.addEventListener("DOMContentLoaded", function() {
+//     printHelloWorld();
+// });
 
-let  x = 10;
-// Here x is 10
+// let  x = 10;
+// // Here x is 10
 
-{  
-let y = 2;
-}
+// {  
+// let y = 2;
+// }
 
-function demo4() {
-    return x;
-}
+// function demo4() {
+//     return x;
+// }
 console.log("Value of x:", demo4());
+
+// Create an Array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
+
+// Display the Array:
+console.log(cars);

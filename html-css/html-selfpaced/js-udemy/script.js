57781f0ec49a3f50257efaf869fd5eba47e91b49
zannager <gzraenga@gmail.com>
2024-05-25 01:19:44 +0800

    practice - basic JS

--- html-css/html-selfpaced/js-udemy/script.js
+++ html-css/html-selfpaced/js-udemy/script.js
@@ -18,3 +18,13 @@ alert (message);
 //         console.error("Element with id 'output' not found.");
 //     }
 // });
+
+function isLeapYear(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}

8cb00d2a40e7957c3cca2d58c391e865ef33057d
zannager <gzraenga@gmail.com>
2024-05-28 02:04:26 +0800

    JS basic

---
+++ html-css/html-selfpaced/js-udemy/lesson2/script.js
@@ -0,0 +1,17 @@
+// for (let i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+// for (let i=1; i<=10; i++) {
+//     if (i==6) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+for (let i=1; i<=10; i++) {
+    if (i==8) {
+        continue;
+    }
+    console.log(i);
+}

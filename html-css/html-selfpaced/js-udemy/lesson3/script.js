3a73f84a4f6b151701be7178c070b3b494126bbb
zannager <gzraenga@gmail.com>
2024-05-29 00:57:14 +0800

    JS basic

---
+++ html-css/html-selfpaced/js-udemy/lesson3/script.js
@@ -0,0 +1,39 @@
+// for (let i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+// for (let i=1; i<=10; i++) {
+//     if (i==6) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+for (let i=1; i<=10; i++) {
+    if (i==8) {
+        continue;
+    }
+    console.log(i);
+}
+
+function is_integer(number) {
+    return Number.isFinite(number) && Math.floor(number) === number;
+}
+
+// function is_integer(number) {
+//     // Check if the number is a finite number
+//     if (typeof number !== 'number' || isNaN(number) || !isFinite(number)) {
+//         return false;
+//     }
+    
+//     // Check if the floor value of the number is equal to the original number
+//     return Math.floor(number) === number;
+// }
+
+function add_all(arr) {
+    let sum = 0;
+    arr.forEach(function(number) {
+        sum += number;
+    });
+    return sum;
+}

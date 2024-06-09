0ef0602618519ae1f24673228de73be44f2aa40b
zannager <gzraenga@gmail.com>
2024-06-09 13:42:58 +0800

    TypeScript

--- session5(TypeScript)/lesson1/script.ts
+++ session5(TypeScript)/lesson1/script.ts
@@ -1,9 +1,18 @@
 interface Person {
     name: string,
     age: number
+    gender: Gender
+}
+
+enum Gender {
+    Male,
+    Female
 }
 
 let person: Person = {
     name: 'Alice',
-    age: 4
+    age: 4,
+    gender: Gender.Male
 }
+
+console.log(person)

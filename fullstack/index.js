937f8288660238887bcb561e1db5f1c5bd437443
zannager <gzraenga@gmail.com>
2024-06-12 02:16:14 +0800

    learning morder JS

--- fullstack/index.js
+++ fullstack/index.js
@@ -2,5 +2,17 @@
 // name = 'zen';
 // alert(name);
 
-const name = 'ryan';
-alert(name);
+
+
+
+// if (true) {
+//     let name = 'ryan';
+//     alert(name);
+// }
+
+let fname = 'ryan';
+let lname = 'd';
+let age = prompt("guess ryan's age..");
+
+let result =`${fname} ${lname} is ${age} years old`;
+alert(result);

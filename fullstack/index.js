79b7f850e3ecf61ade77bb60d331f1f546d29556
zannager <gzraenga@gmail.com>
2024-06-13 02:26:15 +0800

    learning modern JS

--- fullstack/index.js
+++ fullstack/index.js
@@ -77,10 +77,34 @@
 //     console.log(`${name} from ${university}`)
 // }
 
-let uniStudent = ({name, university}) => {
-    console.log(`${name} from ${university}`)
+// let uniStudent = ({name, university}) => {
+//     console.log(`${name} from ${university}`)
+// }
+// uniStudent({
+//     name: 'ryan',
+//     university: 'university of baguio'
+// })
+
+// let [, firstMountain] = ['everest', 'fish tail', 'annaputa'];
+// console.log(firstMountain);
+
+//restructuring
+// var name = 'everest';
+// var height = 8848;
+// var output = function() {
+//     console.log(`Mt. ${this.name} is ${this.height} meter tall`);
+// }
+
+// var adventureClimbing = {name, height, output};
+// adventureClimbing.output();
+
+
+
+var adventureClimbing = {
+    name: 'everest',
+    height: 8848,
+    output() {
+    console.log(`Mt. ${this.name} is ${this.height} meter tall`)
 }
-uniStudent({
-    name: 'ryan',
-    university: 'university of baguio'
-})
+};
+adventureClimbing.output();
